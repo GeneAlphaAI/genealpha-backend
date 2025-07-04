7efd1754a008fd17e15bf62ac6aa360c94de4a8f
BlackNeuron00 <bilal@genealpha.ai>
2025-07-04 18:04:21 +0500

    Configures structured stdout logging

---
+++ logger.py
@@ -0,0 +1,6 @@
+import logging
+import sys
+
+LOG_FORMAT = "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+logging.basicConfig(stream=sys.stdout, level=logging.INFO, format=LOG_FORMAT)
+logger = logging.getLogger("crypto_predictor")
