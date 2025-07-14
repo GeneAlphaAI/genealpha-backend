966a749fb28eef6a340fbf4a1862532924c7f6dc
BlackNeuron00 <bilal@genealpha.ai>
2025-07-14 05:47:42 +0500

    removed the schema from this file

--- db.py
+++ db.py
@@ -15,20 +15,3 @@ def get_db():
         yield db
     finally:
         db.close()
-
-
-# models.py (SQLAlchemy ORM)
-from sqlalchemy import Column, Integer, DateTime, JSON, Float, String
-from db import Base
-import datetime
-
-class Prediction(Base):
-    __tablename__ = "predictions"
-
-    id = Column(Integer, primary_key=True, index=True)
-    token = Column(String, index=True)
-    timestamp = Column(DateTime, default=datetime.datetime.utcnow)
-    features = Column(JSON)
-    xgboost = Column(Float)
-    lightgbm = Column(Float)
-    random_forest = Column(Float)
