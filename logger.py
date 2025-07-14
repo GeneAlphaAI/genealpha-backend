980100f42468eb984c5c121e38f42153af32ef76
BlackNeuron00 <bilal@genealpha.ai>
2025-07-14 07:21:55 +0500

    Updated logger name

--- logger.py
+++ logger.py
@@ -3,4 +3,4 @@ import sys
 
 LOG_FORMAT = "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
 logging.basicConfig(stream=sys.stdout, level=logging.INFO, format=LOG_FORMAT)
-logger = logging.getLogger("crypto_predictor")
+logger = logging.getLogger("gene_alpha")
